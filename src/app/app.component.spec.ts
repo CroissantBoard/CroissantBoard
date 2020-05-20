519187faf63abf2c590735bf059e62f8ed0be3bb
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-05-20 12:18:25 +0300

    Fix typo in the name of the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CroissantBoar'`, () => {
+  it(`should have as title 'CroissantBoard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CroissantBoar');
+    expect(app.title).toEqual('CroissantBoard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('CroissantBoar app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'CroissantBoard app is running!'
+    );
   });
 });
