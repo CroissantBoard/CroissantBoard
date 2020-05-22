04163cd6c82af781716be40b702171f1fab69ba9
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-05-22 22:37:04 +0300

    Implement register page

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 import { AuthService } from 'src/app/core/authentification/auth.service';
+import { fullNameValidator } from '../../shared/validators/input.validator';
 
 @Component({
   selector: 'app-register',
@@ -10,32 +11,61 @@ import { AuthService } from 'src/app/core/authentification/auth.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  registerForm: FormGroup;
+  nameFormControl: FormControl;
+  emailFormControl: FormControl;
+  passwordFormControl: FormControl;
+  confirmPasswordFormControl: FormControl;
   error: string;
 
-  constructor(
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private authService: AuthService
-  ) {
-    this.registerForm = this.formBuilder.group({
-      email: '',
-      password: '',
-      confirmPassword: ''
-    });
+  constructor(private router: Router, private authService: AuthService) {
+    this.nameFormControl = new FormControl('', [
+      Validators.required,
+      fullNameValidator
+    ]);
+    this.emailFormControl = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]);
+    this.passwordFormControl = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]);
+    this.confirmPasswordFormControl = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]);
   }
 
-  async onSubmit({ email, password, confirmPassword }): Promise<boolean> {
-    if (password === confirmPassword) {
-      try {
-        await this.authService.signUp(email, password);
+  async onSubmit(): Promise<boolean> {
+    if (
+      this.nameFormControl.invalid ||
+      this.emailFormControl.invalid ||
+      this.passwordFormControl.invalid ||
+      this.confirmPasswordFormControl.invalid
+    ) {
+      console.log('nope');
+      return;
+    }
 
-        return this.router.navigate(['/todos']);
+    if (
+      this.passwordFormControl.value === this.confirmPasswordFormControl.value
+    ) {
+      try {
+        await this.authService.signUp(
+          this.nameFormControl.value.trim(),
+          this.emailFormControl.value.trim(),
+          this.passwordFormControl.value
+        );
+
+        return this.router.navigate(['/board']);
       } catch (err) {
-        this.error = err.message;
+        console.log(err);
+        if (err.code === 'auth/email-already-in-use') {
+          this.emailFormControl.setErrors({ 'inUse': true });
+        }
       }
     } else {
-      this.error = 'Passwords does not match';
+      this.confirmPasswordFormControl.setErrors({ 'matchPassword': true });
     }
   }
 }
