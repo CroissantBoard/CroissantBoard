91d3c4259b687d1f835d11c4e2c5151e64a383aa
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-05-22 22:36:50 +0300

    Implement login page

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const MaterialComponents = [
     MatButtonModule,
+    MatFormFieldModule,
     MatInputModule,
 ];
 
