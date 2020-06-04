9218cb6da5bde185760d3ca21c177c1f7010eee4
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-06-04 21:46:23 +0300

    Improve age validation

--- src/app/shared/validators/age.validator.ts
+++ src/app/shared/validators/age.validator.ts
@@ -1,27 +1,33 @@
 import { AbstractControl } from '@angular/forms';
 
 export function ageValidator(control: AbstractControl) {
-  const value = control.value.trim();
+  let value = control.value.trim();
 
   if (value === '') {
     return null;
   }
 
-  if (isInt(value)) {
-    return parseInt(value) > 0 ? null : { 'invalidAge': true };
-  } else {
+  if (!isInt(value)) {
     return { 'notInteger': true };
+  } else {
+    value = parseInt(value, 10);
+
+    if (value < 1) {
+      return { 'invalidAge': true };
+    } else if (value < 13) {
+      return { 'tooYoung': true };
+    } else if (value > 112) {
+      return { 'tooOld': true };
+    } else {
+      return null;
+    }
   }
 }
 
 function isInt(value: any) {
-  let x: number;
-
   if (isNaN(value)) {
     return false;
   }
 
-  x = parseFloat(value);
-
-  return (x | 0) === x;
+  return Number.isInteger(parseInt(value, 10));
 }
