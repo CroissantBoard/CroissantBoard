8874b91b4ae91385e8c8cbf79ee0a55ab82d90e5
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-06-04 13:03:25 +0300

    Add route for single user page

--- src/app/modules/board/board-routing.module.ts
+++ src/app/modules/board/board-routing.module.ts
@@ -5,6 +5,7 @@ import { BoardPageComponent } from './board-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { TasksPageComponent } from './pages/tasks-page/tasks-page.component';
 import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 import { MeetingsPageComponent } from './pages/meetings-page/meetings-page.component';
 
 export const routes: Routes = [
@@ -14,6 +15,7 @@ export const routes: Routes = [
     children: [
       { path: 'home', component: HomePageComponent },
       { path: 'tasks', component: TasksPageComponent },
+      { path: 'users/:id', component: UserPageComponent },
       { path: 'users', component: UsersPageComponent },
       { path: 'meetings', component: MeetingsPageComponent },
     ]
