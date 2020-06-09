9b89c1d0f3e79e807b61c23e3379e6bdd58b044a
roma-bjj <roma_tsakhlo@ukr.net>
2020-06-09 11:20:34 +0300

    telegram work

--- tg/index.js
+++ tg/index.js
@@ -1,59 +1,72 @@
 const admin = require('firebase-admin');
 const telegram = require('telegram-bot-api');
-const util = require('util');
 const serviceAccount = require('./serviceAccountKey.json');
+const URL = require('./keys')
+const TOKEN = require('./keys')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: ""
+  databaseURL: URL
 });
 
-const db = admin.database()
+const db = admin.firestore();
 
-const ref = db.ref('users');
-
-ref.on('value', function(snapshot) {
-   
-    snapshot.forEach(function(childSnapshot) {
-
-        let data = childSnapshot.val()
-
-        console.log(data)
-    })
-    });
+const ADD_USER_MSG_PREFIX = "/add_user";
 
 const api = new telegram({
-          token: '',
+          token: TOKEN,
           updates: {
         	enabled: true
             }
   });
 
 api.on('message', (message) => {
-    console.log(message);
-    console.log(message.chat.id);
-    console.log(message.text); 
     
     const chat_id = message.chat.id;
     const chat_text = message.text;
 
-    api.sendMessage({
-        chat_id: chat_id,
-        text: chat_text === '/start' ? 'Write your e-mail' : 'Write /start correct'
-    })
-    .then(data => {
-        console.log(message.text);
-        console.log(util.inspect(data, false, null));
-        const email = message.text;
-
-        if(email == ref) {
-            ref.child(email).set({'tg': chat_id})
-            console.log(true)
-        }
-    })
-    .catch(function(err)
-    {
-        console.log(err);
-    });
+    if (!chat_text.startsWith(ADD_USER_MSG_PREFIX)) {
+        api.sendMessage({
+            chat_id: chat_id,
+            text: chat_text === '/start' ? 'type command /add_user email@gmail.com' : 'Write /start correct'
+        })
+        .catch(function(err)
+        {
+            console.log(err);
+        });
+    }    
 });
 
+api.on('message', (message) => {
+    if (message.text.startsWith(ADD_USER_MSG_PREFIX)) {
+
+        const [,email] = message.text.split(' ');
+
+        db.collection('users').where('email', '==', email).get()
+            .then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    const docRef = db.collection('users').doc(doc.id);
+                    docRef.update({tg: message.chat.id})
+                });
+            })
+            .catch((err) => {
+                console.log('Error getting documents', err);
+            });
+                }
+})
+
+const users = db.collection('users')
+users.onSnapshot(querySnapshot => { let changes = querySnapshot.docChanges(); 
+    for (let change of changes) {
+        const user = change._document._fieldsProto;
+        const tgId = user.tg && user.tg.integerValue;
+        const userName = user.name && user.name.stringValue;
+        
+        if (change.type === 'modified' && tgId) {
+            api.sendMessage({
+                chat_id: tgId,
+                text: `${userName || 'User'} your profile is updated !`
+            })
+        } 
+    } 
+});
